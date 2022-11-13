f017a638422645c61d238de126399e6ae6ef7f33
camille-004 <dunningcamille@gmail.com>
2022-11-13 11:07:08 -0800

    Util plotting function

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List
 
+import matplotlib.pyplot as plt
 import yaml
 
 CONFIG_DIR = "config"
@@ -21,6 +22,41 @@ def load_config(config_name: str) -> Dict:
         Contents of config file.
     """
     with open(Path(CONFIG_DIR, config_name + ".yaml")) as f:
-        config = yaml.safe_load(f)
+        config = yaml.load(f, Loader=yaml.FullLoader)
 
     return config
+
+
+training_config = load_config("training")
+
+
+def plot_reward(
+    _train_rewards: List, _test_rewards: List, _reward_thres: int = None
+) -> None:
+    """
+    Plot the rewards of RL trajectories.
+
+    Parameters
+    ----------
+    _train_rewards : List
+        Train rewards to plot
+    _test_rewards : List
+        Test rewards to plot
+    _reward_thres : int
+        Reward threshold (indciate with red line)
+
+    Returns
+    -------
+    None
+    """
+    plt.figure(figsize=training_config["plot_figsize"])
+    plt.plot(_test_rewards, label="Test Reward")
+    plt.plot(_train_rewards, label="Train Reward")
+    plt.xlabel("Episode", fontsize=training_config["axis_fontsize"])
+    plt.ylabel("Reward", fontsize=training_config["axis_fontsize"])
+    if _reward_thres:
+        plt.hlines(_reward_thres, 0, len(_test_rewards), color="r")
+
+    plt.legend(loc="lower right")
+    plt.grid()
+    plt.show()
